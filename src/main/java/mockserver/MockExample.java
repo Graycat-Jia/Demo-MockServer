1418f1261ac709cbb080238c6c5f389d13ef81c9
Graycat-Jia <2336248929@qq.com>
2022-06-08 16:43:30 +0800

    新增说明2

--- src/main/java/mockserver/MockExample.java
+++ src/main/java/mockserver/MockExample.java
@@ -16,6 +16,11 @@ import static org.mockserver.model.HttpResponse.response;
 public class MockExample {
     public static void main( String[] args){
         System.out.println("mock server running...");
+        /**  启动本地 MockServer 实例并向其发出 HTTP 请求 , 启动了mockserver和客户端
+         *      启动一个 mock server。
+         *      前两个参数是真实服务的 ip 地址和端口号， mock server 一旦发现有请求没有命中事前定义的规则，
+         *      那么就会转发给真实的服务处理。 而第三个参数就是 mock server 自己的端口号了。
+         * */
         ClientAndServer server = new ClientAndServer("localhost", 8080, 1080);
         server.when(
                 request()
@@ -25,17 +30,25 @@ public class MockExample {
                 response()
                         .withBody("mock success.")
         );
-    }
-     /** 上面 new 一个 ClientAndServer("localhost", 8080, 1080) 就是在启动一个 mock server。
-         前两个参数是真实服务的 ip 地址和端口号， mock server 一旦发现有请求没有命中事前定义的规则，
-         那么就会转发给真实的服务处理。 而第三个参数就是 mock server 自己的端口号了。
-      */
 
+        /**
+         *  以下是另一种构造规则的方法。向远程 MockServer 实例发出 HTTP 请求
+         *  这个方法没有启动mock服务器
+         *  第一个参数指定哪里的mock服务器，第二个是端口
+         * */
+        new MockServerClient("192.168.1.45", 1080).when(
+                request()
+                        .withMethod("GET")
+                        .withPath("/receive")
+                ).respond(
+                        response().withBody("mockserver client respone.")
+        );
+    }
 
      /**
-      *     初次之外，mockserver还有很多强大的功能，eg：验证收到多少次重复请求；
+      *     初此之外，mockserver还有很多强大的功能，eg：验证收到多少次重复请求；
       * */
-    public void initMockServer(){
+    public static void initMockServer(){
         new MockServerClient("localhost", 1080).verify(
                 request().withPath("/receive/amount"), VerificationTimes.atLeast(2));
     }
