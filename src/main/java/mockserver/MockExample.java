96a28de328fdd3ad1415389740b5459d2c514f01
Graycat-Jia <2336248929@qq.com>
2022-06-08 15:31:48 +0800

    新增说明

--- src/main/java/mockserver/MockExample.java
+++ src/main/java/mockserver/MockExample.java
@@ -3,6 +3,7 @@ package mockserver;
 
 import org.mockserver.client.MockServerClient;
 import org.mockserver.integration.ClientAndServer;
+import org.mockserver.verify.VerificationTimes;
 
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
@@ -29,10 +30,21 @@ public class MockExample {
          前两个参数是真实服务的 ip 地址和端口号， mock server 一旦发现有请求没有命中事前定义的规则，
          那么就会转发给真实的服务处理。 而第三个参数就是 mock server 自己的端口号了。
       */
-    public void initMockServer(){
 
+
+     /**
+      *     初次之外，mockserver还有很多强大的功能，eg：验证收到多少次重复请求；
+      * */
+    public void initMockServer(){
+        new MockServerClient("localhost", 1080).verify(
+                request().withPath("/receive/amount"), VerificationTimes.atLeast(2));
     }
 
+    /** 还有一种通过命令行运行mock服务的方法，可以在保存在运行期间添加的配置，方法如下：
+         * 1. persistExpectations配置属性需要设置为true
+         * 2. 用于持久化期望的文件路径可以使用persistedExpectationsPath配置属性进行配置
+         * 3. 期望应该使用initialJsonPath配置属性自动初始化
+     * */
 
 
 
